14774ef2c8fc5de54356bc4c86e02c286719ff40
Roshan Sardar <roshansardar805@gmail.com>
2026-03-07 19:38:23 +0545

    javascript 1 days learing code

---
+++ app/app.js
@@ -0,0 +1,136 @@
+// alert("hello world");
+// variable var let and const
+// ES5 VAR
+// ES6 let and const
+
+
+
+// var a = 10;
+// var b = 12;
+// var a = 20;
+// var c = a + b;
+
+// console.log(c);
+
+// let a = 10;
+// let b = 13;
+// a = 20;
+
+// let c = a + b;
+// console.log(c);
+
+
+// const a = 10;
+// const b = 14;
+// const a = 10;
+// const c = a + b;
+// console.log(c)
+
+// Data Types 
+// 1. String, "content" 'content'
+// 2. Number, example => 10 ,10.1
+// 3. Boolean, true or false
+// 4. undefined , not value
+// 5. null , no value
+// 6. object , {}
+// 7. array , []
+
+
+// let a = [];
+
+// console.log(typeof a);
+
+
+// Arithmetic Operator
+
+//  + - * / % ** ++ --
+
+//  Assignment Operator
+
+//  = += -= *= /= %= **= 
+
+// let a = 10;
+
+// a += 12;
+// console.log(a);
+
+//  Logical Operator 
+// And &&  OR || NOT !
+
+// Relational Operator
+
+//  ==  === data type != !== > < >= <=
+
+// let a = 11;
+// let b = 11;
+// let c = a < b;
+
+// console.log(c);
+
+// let a = 10;
+
+// if(a <= 0){
+//     console.log("hello world");
+// } else{
+//     console.log("hello good by")
+// }
+
+// let inputNumber = prompt("Enter a Number");
+
+// console.log(typeof inputNumber);
+
+// let number = parseInt(inputNumber); 
+
+// console.log(typeof number);
+// 1.1, 11 int, 1.1 float
+
+// switch(number){
+//     case 1:
+//         console.log("prompt number is one");
+//         break;
+//     case 2:
+//         console.log("prompt number is two");
+//         break;
+//     case 3:
+//         console.log("prompt number is three");
+//         break;
+//     case 4:
+//         console.log("prompt number is four");
+//         break;
+//     default:
+//         console.log("prompt number is not valid");
+//         break;
+// }
+
+
+// let i;
+// let promptNumber = prompt("Enter a number");
+// // String
+// let number = parseInt(promptNumber);
+
+//  ${number}
+
+
+// for(let i = 1;i <= 10; i++){
+
+//     let result = number * i;
+
+
+//     console.log(`${number} * ${i} = ${result}`);
+// }
+
+// let i = 1;
+// while(i <= 10){
+//     console.log("hello world");
+//     i++;
+// }
+
+
+// let i = 1;
+
+// do{
+//     console.log("hello world");
+//     i++;
+// } while(i >= 10);
+
+
